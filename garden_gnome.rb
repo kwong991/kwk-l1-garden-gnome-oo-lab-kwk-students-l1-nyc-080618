dcaeb2596248e01a17126570e973d45ceb8229b7
kwong991 <kgwong991@gmail.com>
2018-08-10 14:47:03 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -39,7 +39,9 @@ def shout
   puts "GNARLY!!!"
 end
 
-def intro
+def introduce_self
+  return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+end
 end
 
 
